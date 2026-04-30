3dafd5ab60c39464fe93d2cb55451bc2a526c3d9
LoganBrownGU <2641407b@student.gla.ac.uk>
2026-04-30 20:03:01 +0100

    More sensible main

--- software/src/main.c
+++ software/src/main.c
@@ -26,27 +26,26 @@ int main(void) {
     pthread_create(&t, NULL, graph_render_thread, NULL);
 
     pico *p = pico_new();
-    if (p) {
-        for (;;) {
-            int32_t n; 
-            int16_t *samples = pico_gather_samples(p, &n);
-            point *points = graph_get_buffer(_graph); 
+    
+    if (!p) { printf("pico initialisation failed\n"); return -1; }
+    
+    for (;;) {
+        int32_t n; 
+        int16_t *samples = pico_gather_samples(p, &n);
+        point *points = graph_get_buffer(_graph); 
 
-            float div_x = ((float) GRAPH_SIZE_X - 10.0) / ((float) n);
-            float div_y = ((float) GRAPH_SIZE_Y - 10.0) / (2.0 * (float) INT16_MAX);
-            for (int i = 0; i < n; i++) {
-                float y = ((float) samples[i]) * div_y;
-                points[i].y = y;  
-                float x = ((float) i) * div_x;
-                if (i > n - 10) { printf("%f %f\n", x, y); };
-                points[i].x = x;
-            }
+        float div_x = ((float) GRAPH_SIZE_X - 10.0) / ((float) n);
+        float div_y = ((float) GRAPH_SIZE_Y - 10.0) / (2.0 * (float) INT16_MAX);
+        for (int i = 0; i < n; i++) {
+            float y = ((float) samples[i]) * div_y;
+            points[i].y = y;  
+            float x = ((float) i) * div_x;
+            if (i > n - 10) { printf("%f %f\n", x, y); };
+            points[i].x = x;
         }
-        pico_destroy(&p);
-    } else {
-        printf("pico initialisation failed\n");
     }
-
+    
+    pico_destroy(&p);
     pthread_join(t, NULL);
     
     return 0;

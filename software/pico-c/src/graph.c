627e87236174c5a806a90527480da6bc75ae4cb6
LoganBrownGU <2641407b@student.gla.ac.uk>
2026-05-13 11:54:12 +0100

    c directory

---
+++ software/pico-c/src/graph.c
@@ -0,0 +1,49 @@
+#include "graph.h"
+#include "gl.h"
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <cairo/cairo-xlib.h>
+#include <cairo/cairo.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+GL_Point points[GRAPH_MAX_POINTS];
+int is_running = 1;
+
+static void render_frame(void) {
+    GL_ClearWindow();
+    GL_DrawLines(points, GRAPH_MAX_POINTS);
+}
+
+static void expose_handler(void) {
+    GL_SetWindowVisible();
+}
+
+static void update_state(void) {
+    
+}
+
+
+void graph_init(const char *name) {
+    GL_Init();
+    GL_CreateWindow(GRAPH_SIZE_X, GRAPH_SIZE_Y, GL.color.black);
+    GL_SetWindowTitle(name);
+    GL_SetWindowFixed();
+    GL_SetFont("10x20");
+
+    GL_SetExposeHandler(expose_handler);
+    GL_SetFrameRate(60);
+    GL_Loop(update_state, render_frame);
+    printf("window closing...\n");
+    GL_Quit();
+    is_running = 0;
+}
+
+GL_Point *graph_get_buffer(void) {
+    return points;
+}
+
+int graph_is_running(void) {
+    return is_running;
+}
